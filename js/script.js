8575a5e023904ef62b3cf101913166744e06f892
avto8 <43859197+avto8@users.noreply.github.com>
2018-11-09 08:25:22 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-var modalConfirm = function(callback){
-    $("#btn-callback-carousel").on("click", function(){
+     $("#btn-callback-carousel").on("click", function(){
         $("#callback-modal").modal('show');
       });
     $("#btn-callback-carousel2").on("click", function(){
-      $("#callback-modal").modal('show');
-    });
-    $("#btn-callback-carousel3").on("click", function(){
       $("#callback-modal").modal('show');
     });
     $("#btn-callback-carousel4").on("click", function(){
@@ -18,68 +14,27 @@ var modalConfirm = function(callback){
       $("#callback-modal").modal('show');
     });
 
-    $("#btn-callback-carousel-ok").on("click", function(){
-      callback(true);
-      $("#callback-modal").modal('hide');
-      sendEmailCallBack('inputName', 'inputNumber', false);
+    $(document.body).on('hide.bs.modal,hidden.bs.modal', function () {
+      $('body').css('padding-right','0');
     });
 
-    $("#success-modal-ok2").on("click", function(){
-      callback(false);
-      $("#success-modal2").modal('hide');
-    });
-  };
-  
-  modalConfirm(function(confirm){
-    if(confirm){
-      
-    }else{
-      
-    }
-  });
-
-  function sendEmailCallBack(nameId, phoneId, flag) {
-    var name = document.getElementById(nameId).value;
-    var phone = document.getElementById(phoneId).value;
-    var currentDate = new Date(); 
-    var date = currentDate.getDate() + "." + (currentDate.getMonth()+1) + "." + currentDate.getFullYear()
-    + " время заказа " + currentDate.getHours() + ":"+ (currentDate.getMinutes()<10?'0':'') + currentDate.getMinutes()
-
-    if((typeof name !== 'undefined') && (typeof phone !== 'undefined') && (name !== '') && (phone !== '')) {
-      emailjs.send("default_service","15",{name: name, phone: phone, date: date});
-      if(flag){
-        $("#success-modal").modal('show');
-      }
-      $("#success-modal-ok").on("click", function(){
-        $("#success-modal").modal('hide');
-      });
-    } else {
-      $("#errFields-modal").modal('show');
-      $("#errFields-modal-ok").on("click", function(){
-        $("#errFields-modal").modal('hide');
-      });
-    }
-  }
-
-  function sendEmailQuestions() {
-    var name = document.getElementById('nameUser').value;
-    var email = document.getElementById('emailUser').value;
-    var message = document.getElementById('messageUser').value;
-    var currentDate = new Date(); 
-    var date = currentDate.getDate() + "." + (currentDate.getMonth()+1) + "." + currentDate.getFullYear()
-    + " " + currentDate.getHours() + ":"+ (currentDate.getMinutes()<10?'0':'') + currentDate.getMinutes();
-
-    if((typeof name !== 'undefined') && (typeof email !== 'undefined') && (typeof message !== 'undefined') && (name !== '') && (email !== '') && (message !== '')) {
-      emailjs.send("default_service","16",{name: name, email: email, message: message, date: date});
-      $("#success-modal").modal('show');
-      $("#success-modal-ok").on("click", function(){
-        $("#success-modal").modal('hide');
-      });
-    } else {
-      $("#errFields-modal").modal('show');
-      $("#errFields-modal-ok").on("click", function(){
-        $("#errFields-modal").modal('hide');
-      });
-    }
-  }
+  window.setInterval(function() {
+    function getDateNow() {
+      var n =  new Date();
+      var y = n.getFullYear();
+      var m = n.getMonth() + 1;
+      m = m.toString().length < 2 ? '0'+m : m;
+      var d = n.getDate();
+      d = d.toString().length < 2 ? '0'+d : d;
+      var h = n.getHours();
+      h = h.toString().length < 2 ? '0'+h : h;
+      var min = n.getMinutes();
+      min = min.toString().length < 2 ? '0'+min : min;
+      $('#inputTime').val(d + '-' + m + '-' + y + ' ' + h + ':' + min);
+      $('#inputTime2').val(d + '-' + m + '-' + y + ' ' + h + ':' + min);
+      $('#inputTime3').val(d + '-' + m + '-' + y + ' ' + h + ':' + min);
+      $('#inputTime4').val(d + '-' + m + '-' + y + ' ' + h + ':' + min);
+    };
+    getDateNow();
+  }, 5000);
  
